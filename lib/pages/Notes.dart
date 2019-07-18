442f749dc9016bc3f23e43e0051788181b2d2418
daeh <daeh@daehs-MBP.fritz.box>
2019-07-18 08:38:13 +0200

    added ability to collect notes

--- lib/pages/Notes.dart
+++ lib/pages/Notes.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:gonotes/api/NoteService.dart';
 import 'package:gonotes/models/Note.dart';
 import 'package:gonotes/state/appState.dart';
+import 'package:loading/indicator/ball_beat_indicator.dart';
+import 'package:loading/loading.dart';
 
 class NotesTab extends StatefulWidget {
   NotesTab();
@@ -15,15 +17,16 @@ class NotesTab extends StatefulWidget {
 class _NotesTabState extends State<NotesTab> {
   String test = "hello";
   final List<Note> _collectedNotes = [];
+  bool _isLoading = true;
 
   @override
-  // TODO: implement context
   BuildContext get context => super.context;
 
   void didChangeDependencies() async {
     // empty the notes before pushing in new ones
     _collectedNotes.clear();
     fetchMyCollectedNotes().then((collectedNotes) {
+      setState(() { _isLoading = false; });
       for (var note in collectedNotes) {
         var lat = note["lat"];
         // checking the types
@@ -35,33 +38,42 @@ class _NotesTabState extends State<NotesTab> {
         if (long.runtimeType == int) {
           long = long + .0;
         }
-
         Note buildNote =
             new Note(lat: lat, long: long, note: note["note"], id: note["id"]);
         _collectedNotes.add(buildNote);
       }
-      print(_collectedNotes);
     });
   }
+
+  List<Widget> _buildList() {
+    print(_collectedNotes);
+    List<Widget> list = new List<Widget>();
+    for (var note in _collectedNotes) {
+      print(note.note);
+       list.add(ListTile(title: Text(note.note)));
+    }
+    return list;
+  }
+
+  Widget _buildChild() {
+  if (_isLoading) {
+    return Container(
+        color: Colors.teal,
+        child: Center(
+          // child: Loading(indicator: BallBeatIndicator(), size: 100.0),
+          child: Text("LOADING"),
+        ),
+      );
+  }
+  return ListView(
+  children: _buildList(),
+);
+}
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        body: ListView(
-      children: <Widget>[
-        ListTile(
-          leading: Icon(Icons.map),
-          title: Text("test"),
-        ),
-        ListTile(
-          leading: Icon(Icons.photo_album),
-          title: Text('Album'),
-        ),
-        ListTile(
-          leading: Icon(Icons.phone),
-          title: Text('Phone'),
-        ),
-      ],
-    ));
+        body: Container(child: _buildChild())
+        );
   }
 }
